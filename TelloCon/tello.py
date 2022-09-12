3a87193a1ba864caf0d6a9060418304d55d2284e
Sumiet Gore <sumeetgore96@gmail.com>
2022-09-12 20:39:50 +0300

    Created basic structure for the library

--- TelloCon/tello.py
+++ TelloCon/tello.py
@@ -1,6 +1,40 @@
 """
-A small python library to control DJI Tello Drones
+A simple python library to control DJI Tello Drones
 """
 
 import socket
 from threading import Thread
+
+class Controller:
+
+    """
+    
+    """
+
+    def __init__(self, isSwarm = False):
+        self.isSwarm = isSwarm
+
+        #Store all tello instances in an array
+        self.tellos = {}
+
+    def add(host):
+        tello = Tello()
+
+        self.tellos.update({id:tello})
+
+
+class Tello:
+    """
+
+    """
+    def __init__(self):
+        pass
+
+    def connect():
+        pass
+
+    def disconnect():
+        pass
+
+    def end():
+        pass
